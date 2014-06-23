680f43be4973357577cfde41ee403effad972c81
Joseph Baena <jbaena@myth31.Stanford.EDU>
2014-06-23 14:49:22 -0700

    first commit

---
+++ js/introHCI.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+});
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	$(".likeBtn").click(function(e) {
+		ga("send", "event", "like", "click");
+	});
+}
